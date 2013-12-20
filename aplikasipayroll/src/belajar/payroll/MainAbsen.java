fe136d945ccef6efd50f6d4e1609066e13654b3a
agussaputra3108 <putratarihoran@gmail.com>
2013-12-20 19:18:29 +0700

    REVISI TUGAS PAYROLL PEMROGRAMAN I

---
+++ aplikasipayroll/src/belajar/payroll/MainAbsen.java
@@ -0,0 +1,26 @@
+package belajar.payroll;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+
+public class MainAbsen {
+    public static void main(String[] xx) throws Exception {
+        String namafilePegawai = ("D:/aplikasipayroll/data/tarif.txt");
+        String namafileAbsen = ("D:/aplikasipayroll/data/absen11.txt");
+        
+        PegawaiDao pd = new PegawaiDao(namafilePegawai);
+        AbsenDao ad = new AbsenDao(namafileAbsen, pd);
+        List<Absen> daftarAbsen = ad.semuaData();
+        
+        for(Absen px : daftarAbsen){
+            System.out.println("===========================================");
+            System.out.println("Nomer Pegawai: "+px.getPegawai().getNomer());
+            System.out.println("Nama Pegawai : "+px.getPegawai().getNama());
+            System.out.println("Gaji Pokok   : "+px.getPegawai().getGaji());
+            System.out.println("Jam Datang   : "+px.getDatang());
+            System.out.println("Jam Pulang   : "+px.getPulang());
+			System.out.println("===========================================");
+        }
+    }
+}
---
+++ aplikasipayroll/src/belajar/payroll/MainPegawai.java
@@ -0,0 +1,23 @@
+package belajar.payroll;
+
+import java.io.File;
+import java.util.List;
+
+public class MainPegawai {
+    public static void main(String[] xx) throws Exception {
+    
+        PegawaiDao pd = new PegawaiDao("D:/aplikasipayroll/data/tarif.txt");
+        List<Pegawai> daftarPegawai = pd.semuaPegawai();
+        
+        for(Pegawai px : daftarPegawai){
+            System.out.println("==================================");
+            System.out.println("Nomer     : "+px.getNomer());
+            System.out.println("Nama      : "+px.getNama());
+            System.out.println("Gaji      : "+px.getGaji());
+            System.out.println("Denda     : "+px.getDenda());
+            System.out.println("Lembur    : "+px.getLembur());
+            System.out.println("Transport : "+px.getTransport());
+			System.out.println("==================================");
+        }
+    }
+}
